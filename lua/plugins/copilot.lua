bcefe3872bcfc158bd7fe064380209f02e759de6
Alan <alan@wamser.com>
2024-09-04 14:39:49 -0500

    updates

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,4 @@
+-- TODO: move to completions
 return {
   "github/copilot.vim",
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,26 +1,20 @@
 return {
 	{
 		"williamboman/mason.nvim",
-    lazy = "false",
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-    lazy = "false",
 		config = function()
-      opts = {
-        auto_install = true,
-      }
-	--		require("mason-lspconfig").setup({
-	--			ensure_installed = { "lua_ls", "tsserver" },
-	--		})
+  		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "tsserver" },
+		})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = "false",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
