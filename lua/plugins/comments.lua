fc4c6540c5fe6e63468a4aba8e961a324f0bd690
Alan <alan@wamser.com>
2024-10-07 08:38:33 -0500

    swift comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -4,42 +4,18 @@
 return {
   "numToStr/Comment.nvim",
   config = function()
-    -- require("Comment").setup({
-    -- pre_hook = function(ctx)
-    --     local U = require("Comment.utils")
-    --
-    --     -- Detect the filetype as Swift
-    --     if vim.bo.filetype == "swift" then
-    --       local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
-    --       local location = nil
-    --       if ctx.ctype == U.ctype.block then
-    --         location = require("ts_context_commentstring.utils").get_cursor_location()
-    --       end
-    --       return require("ts_context_commentstring.internal").calculate_commentstring({
-    --         key = type,
-    --         location = location,
-    --       })
-    --     end
-    --   end,
-    -- })
     require("Comment").setup({
-      -- Add a hook to recognize Swift file types
       pre_hook = function(ctx)
         local U = require("Comment.utils")
-
+        -- Check if filetype is Swift
         if vim.bo.filetype == "swift" then
-          -- Determine whether to use linewise (single line //) or blockwise (/* */)
-          local location = nil
-          if ctx.ctype == U.ctype.block then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-          elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
+          -- If the context is blockwise (visual selection or operator pending blockwise), use /* */
+          if ctx.ctype == U.ctype.block or ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            return "/* %s */"
+          else
+            -- For linewise comments, use //
+            return "// %s"
           end
-
-          return require("ts_context_commentstring.internal").calculate_commentstring({
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-          })
         end
       end,
     })
