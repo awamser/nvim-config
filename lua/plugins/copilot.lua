5f3e4cbf3815ac8a72a139dc8a499cee4ef9d8a6
Alan <alan@wamser.com>
2024-12-24 09:08:26 -0600

    avante

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,9 @@
 return {
 	"github/copilot.vim",
+	event = "VimEnter",
+	init = function()
+		vim.g.copilot_no_tab_map = false
+		vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+	end,
+	keys = {},
 }
