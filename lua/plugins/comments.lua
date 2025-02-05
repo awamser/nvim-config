c3a85f1e83a5762326da6bb7dc1ed469048a4a8c
Alan <alan@wamser.com>
2025-02-05 14:04:03 -0600

    comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -7,17 +7,45 @@ return {
     require("Comment").setup({
       pre_hook = function(ctx)
         local U = require("Comment.utils")
-        -- Check if filetype is Swift
-        if vim.bo.filetype == "swift" then
-          -- If the context is blockwise (visual selection or operator pending blockwise), use /* */
+
+        -- Get the current filetype
+        local ft = vim.bo.filetype
+
+        -- Handle Swift comments
+        if ft == "swift" then
           if ctx.ctype == U.ctype.block or ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
             return "/* %s */"
           else
-            -- For linewise comments, use //
+            return "// %s"
+          end
+        end
+
+        -- Handle HTML comments
+        if ft == "html" then
+          return "<!-- %s -->"
+        end
+
+        -- Handle embedded JavaScript/CSS in HTML
+        if ft == "javascript" or ft == "css" then
+          if ctx.ctype == U.ctype.block or ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            return "/* %s */"
+          else
             return "// %s"
           end
         end
       end,
+
+      -- Additional language-specific settings
+      toggler = {
+        line = "gcc",
+        block = "gbc",
+      },
+      opleader = {
+        line = "gc",
+        block = "gb",
+      },
+      -- Enable extra space after comment delimiter
+      padding = true,
     })
   end,
 }
