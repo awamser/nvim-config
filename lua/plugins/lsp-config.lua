44be7183c0b965765845929b58f149fbef5e16a1
Alan <alan@wamser.com>
2025-02-23 11:49:05 -0600

    sync

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -45,6 +45,12 @@ return {
       vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
       vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
       vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set(
+        "n",
+        "<leader>xd",
+        "<cmd>Telescope diagnostics<CR>",
+        { desc = "Show diagnostics (Telescope)" }
+      )
     end,
   },
 }
