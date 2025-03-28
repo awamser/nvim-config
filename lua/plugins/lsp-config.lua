b541cbaa1a987a58d2aa6a3b7796031514078d87
Alan <alan@wamser.com>
2025-03-28 15:17:12 -0500

    leptos

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,58 +1,72 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "html" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "html" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.sourcekit.setup({
-        filetypes = { "swift", "objc", "objcpp", "cuda", "proto" },
-        -- capabilities = capabilities,
-        capabilities = vim.tbl_extend("force", capabilities, {
-          worksapce = {
-            didChangeWatchedFiles = {
-              dynamicRegistration = true,
-            },
-          },
-        }),
-      })
+			lspconfig.rust_analyzer.setup({
+				settings = {
+					["rust-analyzer"] = {
+						procMacro = {
+							ignored = {
+								leptos_macro = {
+									"server",
+								},
+							},
+						},
+					},
+				},
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-      vim.keymap.set(
-        "n",
-        "<leader>xd",
-        "<cmd>Telescope diagnostics<CR>",
-        { desc = "Show diagnostics (Telescope)" }
-      )
-    end,
-  },
+			lspconfig.sourcekit.setup({
+				filetypes = { "swift", "objc", "objcpp", "cuda", "proto" },
+				-- capabilities = capabilities,
+				capabilities = vim.tbl_extend("force", capabilities, {
+					worksapce = {
+						didChangeWatchedFiles = {
+							dynamicRegistration = true,
+						},
+					},
+				}),
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set(
+				"n",
+				"<leader>xd",
+				"<cmd>Telescope diagnostics<CR>",
+				{ desc = "Show diagnostics (Telescope)" }
+			)
+		end,
+	},
 }
