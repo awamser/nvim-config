114f4245fc61881025c4fd0712c3cfc04f255b84
Alan <alan@wamser.com>
2025-05-05 15:26:33 -0500

    onedark

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,9 +2,11 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 return {
 	"nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
 	config = function()
 		require("lualine").setup({
 			options = {
+        icons_enabled = true,
 				theme = "onedark",
 			},
 		})
