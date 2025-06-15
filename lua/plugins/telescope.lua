3f2a1b7b02c71ad0e21beab93b64317ed36f9616
Alan Wamser <alan@wamser.com>
2025-06-15 18:22:38 -0500

    buffer delete

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,45 @@
 return {
   {
-    "nvim-telescope/telescope.nvim", tag = "0.1.8",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    end
+      local builtin = require("telescope.builtin")
+      local actions = require("telescope.actions")
+
+      require("telescope").setup({
+        pickers = {
+          buffers = {
+            previewer = false, -- 💡 Needed to make delete_buffer work
+            sort_mru = true,
+            mappings = {
+              i = {
+                ["<C-d>"] = actions.delete_buffer,
+              },
+              n = {
+                ["<C-d>"] = actions.delete_buffer,
+              },
+            },
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup {
+      require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
